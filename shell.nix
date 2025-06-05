44b9361e1742e2b6baeacb00b66cb4b5a9218471
ben <ben.uzh@proton.me>
2025-06-05 02:04:40 +0200

    pin a nixpkgs version

--- shell.nix
+++ shell.nix
@@ -1,26 +1,29 @@
 # shell.nix
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-24.05.tar.gz";
+    sha256 = "0zydsqiaz8qi4zd63zsb2gij2p614cgkcaisnk11wjy3nmiq0x1s";
+  }) {} }:
 
 let
   # Import package definitions from default.nix
   defs = import ./nix/default.nix { inherit pkgs; };
-  
+
   # Extract variables from the imported definitions
   inherit (defs) rPackageList rEnv pythonEnv rProfileContent;
-  
+
   # Create a shell script to launch RStudio with the right environment
   rstudioWrapper = pkgs.writeScriptBin "rstudio-wrapper" ''
     #!${pkgs.stdenv.shell}
-    
+
     # Create .Rprofile if it doesn't exist
     if [ ! -f .Rprofile ]; then
       echo '${rProfileContent}' > .Rprofile
     fi
-    
+
     # Launch RStudio with the right environment
     exec ${pkgs.rstudio}/bin/rstudio "$@"
   '';
-  
+
 in pkgs.mkShell {
   buildInputs = [
     pkgs.R
@@ -29,11 +32,11 @@ in pkgs.mkShell {
     rstudioWrapper
     pythonEnv
   ];
-  
+
   shellHook = ''
     # Create or update .Rprofile in the project directory
     echo '${rProfileContent}' > .Rprofile
-    
+
     # Add information about the environment
     echo "=== R Environment Setup ==="
     echo "Custom .Rprofile has been created"
